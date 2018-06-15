da109971f8c92d6d68ee544047946aa61b0d73db
怡君 陳 <et599y@yahoo.com.tw>
2018-06-15 23:15:27 +0800

    6/15

--- map.js
+++ map.js
@@ -3,12 +3,18 @@ var centerMark = {
     lat: 24.803049,
     lng: 120.972016
 };
+//標籤
+var markers=[];
 var lastWindow=null;
+//站點資料
+var Rdata;
 
 var directionsDisplay;
 var directionsService;
 var oldDirections = [];
 var currentDirections = null;
+
+var language= "繁";
 
 //初始化地圖
 function initMap() {
@@ -51,7 +57,8 @@ function initMap() {
     //取得站點資料   
     $.get('http://opendata.hccg.gov.tw/dataset/1f334249-9b55-4c42-aec1-5a8a8b5e07ca/resource/4d5edb22-a15e-4097-8635-8e32f7db601a/download/20180212143756340.json', function(stationRecord){
         console.log(stationRecord)
-        setMarkers(map, stationRecord);
+        Rdata = stationRecord;
+        setMarkers(map, Rdata);
     });  
 
     //路徑規劃
@@ -83,7 +90,7 @@ function Getposition() {
 
 //點位
 function setMarkers(map, locations){
-    var marker, i;
+    var i;
     for(i=0; i<locations.length; i++){
         var loan = locations[i]['站點名稱']
         var lat = parseFloat(locations[i]['緯度'])
@@ -95,6 +102,8 @@ function setMarkers(map, locations){
         var marker = new google.maps.Marker({  
           map: map, title: loan , position: latlngset , icon: 'image/icon.png'
         });
+
+        markers.push(marker);
 
         var content = "<h3>站點名稱: " + loan +  '</h3>' + "站點位置: " + add + "<br><br><button onclick=" + "calcRoute('" + loan + "')"+ ">規劃路徑</button>"    
         var infowindow = new google.maps.InfoWindow()
@@ -125,4 +134,58 @@ function calcRoute(pEnd) {
       }
     });
 		
+}
+
+//清空標記
+function setMapOnAll() {
+    for (var i = 0; i < markers.length; i++) {
+        markers[i].setMap(null);
+        console.log("remove")
+    }
+    markers = [];
+}
+
+//多語系
+function toChinese(){
+    setMapOnAll();
+    console.log(Rdata)
+    //繁轉簡
+    var state=false;
+    if(language == "繁"){
+        for(var x=0;x<Rdata.length;x++){
+            console.log(Rdata[x]['站點名稱']);
+            // $.ajax({
+            //     url: "http://www.webxml.com.cn/WebServices/TraditionalSimplifiedWebService.asmx/toSimplifiedChinese?sText=站點名稱",
+            //     type: "GET",
+            //     dataType: 'jsonp',
+            //     success: function(data){
+            //         console.log("data", data);
+            //     }
+            //   });
+            (function(index){
+                $.get("https://cors.io/?http://www.webxml.com.cn/WebServices/TraditionalSimplifiedWebService.asmx/toSimplifiedChinese?sText=" + Rdata[x]['站點名稱'], function(data){
+                    // data = data.replace(//, "");
+
+                    console.log("data", data);
+                    Rdata[index]['站點名稱'] = data;
+                });
+
+                $.get("https://cors.io/?http://www.webxml.com.cn/WebServices/TraditionalSimplifiedWebService.asmx/toSimplifiedChinese?sText=" + Rdata[x]['站點位置'], function(data){
+                    console.log("data2", data);
+                    Rdata[index]['站點位置'] = data;
+                    state = true;
+                });
+            })(x);
+            
+            
+            
+        }   
+        
+        if(state == true){
+            language = "簡";
+            setMarkers(map, Rdata);
+            console.log("setmark")
+        }
+        
+    }
 }
